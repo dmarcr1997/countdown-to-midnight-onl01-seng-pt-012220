070a9cb6cf1822f7daf9ac23df5d97a370156dba
Damon R <dmarcr1997@gmail.com>
2020-01-22 23:18:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,4 +8,10 @@ def countdown(num)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(num)
+  while(num!=0)
+    puts "#{num} SECOND(S)!"
+    num-=1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
